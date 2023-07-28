c536e0623b1ddea2d24830c3b0bd0bc6bf3954af
Łukasz Kocela <117775643+lukaszkocela@users.noreply.github.com>
2023-07-28 14:39:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <React.StrictMode>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/react-homework-template">
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
